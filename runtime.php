70410eb0e24ec784f746613a0d9faadc03c59dff
hemacarun <hema.gowda88@gmail.com>
2015-03-09 21:52:12 +0530

    Update runtime.php

--- runtime.php
+++ runtime.php
@@ -1,4 +1,28 @@
 <?php
+//
+//
+// This software is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This Moodle block is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The PHP sandbox
+ *
+ * It provide the runtime environment to run php script
+ *
+ * @package resource
+ * @copyright 2015 hemalatha c arun < hemalatha@eabyas.in >
+* @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
 
 require_once('../../config.php');
 global $CFG, $DB, $OUTPUT, $PAGE;
